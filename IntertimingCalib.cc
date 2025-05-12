952532ec6e2ca769e3e86cfa26b7c6aec4702b0d
Grace <gtytus@hawaii.edu>
2025-05-12 12:57:07 -1000

    debugging

--- IntertimingCalib.cc
+++ IntertimingCalib.cc
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <string>
 #include <boost/format.hpp>
+#include "CAnalysisManager.hh"
+
 
 using std::vector, std::cout, std::endl;
 
@@ -12,32 +14,44 @@ using std::vector, std::cout, std::endl;
 int main(int argc, char * argv[])
 {
 
-  //prepare root files to be read
-  char FilenameRoot[400];
-  sprintf(FilenameRoot,"%s/%s*.root",argv[1], argv[2]); 
-
   //prepare reconstronstructed event
   CEventRec* Event = new CEventRec;
   TChain * TreeRec = new TChain("TreeRec");
   TreeRec->SetBranchAddress("Rec", &Event);
-  TreeRec->Add(FilenameRoot);
+  TreeRec->Add(argv[1]);
   TreeRec->GetEntry(0);
 
-  //constructing histogram
-  TH1D* Ht_diff = new TH1D("Ht_diff", "t_66_t_667", 150,-5,5);
+  //constructing t_diff histogram
+  TH1D* Ht_diff = new TH1D("Ht_diff", "t_66_t_67", 150,-5,5);
   Ht_diff->SetLineWidth(3);
   Ht_diff->SetLineColor(kBlue);
   Ht_diff->SetXTitle("nanoseconds");
   Ht_diff->SetYTitle("n");
 
-  //declaring variables
+  //constructing NTracks histogram
+  TH1D* HNTracks = new TH1D("HNTracks", "n tracks", 150, -5, 5);
+  HNTracks->SetLineWidth(3);
+  HNTracks->SetLineColor(kRed);
+  HNTracks->SetXTitle("n tracks");
+  HNTracks->SetYTitle("n");
+
+  //populating NTracks histogram
+  for (unsigned int i=0; i< TreeRec->GetEntries(); i++) {
+    HNTracks ->Fill(Event->GetNTracks());
+    cout << "NEntries " << TreeRec->GetEntries() << std::endl;
+    if (Event->GetNTracks() != 1){
+      continue;
+    }
+  }
+
+  //declaring variables for cuts
   int primary_vid   = 100000500;
   int secondary_vid = 100000600;
   int n_rejected    = 0;
   int n_accepted    = 0;
   int n_histo       = 0;
 
-
+  //cuts
   for (uint i = 0; i < TreeRec->GetEntries(); i++) {
     TreeRec->GetEntry(i); 
       
@@ -58,15 +72,18 @@ int main(int argc, char * argv[])
     }
     n_accepted++;
 
+    //populating t_diff histogram
     double primary_time   = -1;
     double secondary_time = -1;
     if (Event->GetNTracks() == 1) {
       for(unsigned int k = 0; k < Event->GetTotalEnergyDeposition().size(); k++) {
         unsigned int VolumeId = Event->GetVolumeId().at(k);
+        //debugging
         //cout << " -- volumeid " << VolumeId << endl;
         if (VolumeId >= 200000000) {
           continue;
         }
+        //debugging
         //cout << " -- time " << Event->GetTime().at(k) << endl;
         if(VolumeId == primary_vid){
             primary_time = Event->GetTime().at(k);
@@ -78,6 +95,8 @@ int main(int argc, char * argv[])
             continue;
           }  
           t_diff = primary_time - secondary_time; 
+          cout << "t_diff " << TreeRec->GetEntries() << std::endl;
+          //debugging
           //cout << "  ==> primary time " << primary_time << " secondary time " << secondary_time << " t diff " << t_diff << endl;
           Ht_diff->Fill(t_diff);
           n_histo++;
@@ -86,13 +105,31 @@ int main(int argc, char * argv[])
       }
     }
   }
+
+  //canvas for t_diff histogram
   TCanvas* t_diff_canvas = new TCanvas("Ht_diff", "Ht_diff", 200, 10, 900, 900);
   t_diff_canvas->SetLeftMargin(0.11);
   t_diff_canvas->SetRightMargin(0.04);
-  t_diff_canvas->SetTopMargin(0.04);
+  t_diff_canvas->SetTopMargin(0.08);
   t_diff_canvas->SetLogy();
   Ht_diff->Draw("HIST");
   std::string filename = (boost::format("%1%_%2%_t_diff.pdf") % primary_vid % secondary_vid).str();
   t_diff_canvas->SaveAs(filename.c_str());
+
+  //canvas for NTracks histogram
+  TCanvas* CNTracks = new TCanvas("HNTracks", "HNTracks", 200, 10, 900, 900);
+  CNTracks->SetLeftMargin(0.11);
+  CNTracks->SetRightMargin(0.04);
+  CNTracks->SetTopMargin(0.08);
+  CNTracks->SetLogy();
+  HNTracks->Draw("HIST");
+  std::string filename1 = (boost::format("%1%_%2%_ntracks.pdf") % primary_vid % secondary_vid).str();
+  CNTracks->SaveAs(filename1.c_str());
+
+  //checks
+  cout << n_rejected << " events did not pass our cut!" << endl;
+  cout << n_accepted << " passed our cut!" << endl;
+  cout << n_histo << " events were histogrammed!" << endl;
+  return EXIT_SUCCESS;
 
 }
